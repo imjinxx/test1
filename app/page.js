5c2b48174739036a234f7dd815edbcc232247a35
imjinxx <someday154254@gmail.com>
2024-06-20 09:18:42 +0700

    first commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Navbar from '../app/component/Navbar.js';
+
+export default function Home() {
+  return (
+
+        <>
+        <Navbar />
+
+        <h1> Page1</h1>
+
+        </>
+
+  );
+}
