b2f83503fbbfee0aae4cb282a887d896a58453b3
imjinxx <someday154254@gmail.com>
2024-08-15 14:16:47 +0700

    new

---
+++ app/service/page.js
@@ -0,0 +1,11 @@
+import Navber from "../component/Navbar.js"
+
+export default function Home() {
+  return (
+
+    <>
+      <Navber /> <h1>service</h1>
+    </>
+
+  );
+}
---
+++ app/users/page.js
@@ -0,0 +1,73 @@
+'use client';
+import Link from 'next/link'
+
+import { useEffect, useState } from 'react';
+
+export default async function Page() {
+  const res = await fetch(`http://localhost:3001/api/users`);
+  const items = await res.json();
+  /*
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        const res = await fetch('http://localhost:3000/api/users');
+        if (!res.ok) {
+          console.error('Failed to fetch data');
+          return;
+        }
+        const data = await res.json();
+        setItems(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+ 
+  getUsers()
+  const interval  = setInterval(getUsers, 1000);
+  return () => clearInterval(interval );
+}, []);
+*/
+
+  return (
+    <>
+
+    <br /><br /><br /><br />
+    <div className="container">
+      <div class="card">
+  <div class="card-header">
+    Users List
+  </div>
+  <div class="card-body">
+  <div className="row">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th className='col-md-2 text-center'>#</th>
+            <th className='col-md-4'>Firstname</th>
+            <th className='col-md-4'>Lastname</th>
+            <th className='col-md-1'>Eidt</th>
+            <th className='col-md-1'>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => (
+            <tr key={item.id}>
+              <td className='text-center'>{item.id}</td>
+              <td>{item.firstname}</td>
+              <td>{item.lastname}</td>
+              <td><Link href={`/users/edit/${item.id}`} className="btn btn-warning">Edit</Link></td>
+              <td><Link href={`/users/del/${item.id}`} className="btn btn-danger">Del</Link></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    </div>
+
+    </div>
+    </div>
+    <br /><br />
+
+    </>
+  );
+}
